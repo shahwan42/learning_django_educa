3c9e2a10656734fc7d54376365178ade870cc7d0
Ahmed Shahwan <ahmed@shahwan.me>
2019-03-10 12:40:24 +0200

    content models

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class Subject(models.Model):
@@ -35,3 +37,51 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Content(models.Model):
+    # the module contains multiple contents (with different content types: images, videos, text, etc)
+    # so we need to setup a generic relation using the ContentTypes framework
+    module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
+    # setting up the generic relation requires 3 steps
+    # 1. a ForeingKey to the ContentType model
+    content_type = models.ForeignKey(
+        ContentType, on_delete=models.CASCADE, limit_choices_to={'model__in': ('text', 'video', 'image', 'file')})
+    # 2. field to store the primary key of the related object
+    object_id = models.PositiveIntegerField()
+    # 3. a GenericForeignKey to the related object by combining the two previous fields
+    item = GenericForeignKey('content_type', 'object_id')
+
+
+class ItemBase(models.Model):
+    """Abstract model for common fields in each content item"""
+    owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    """Text items"""
+    content = models.TextField()
+
+
+class File(ItemBase):
+    """File items"""
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    """Image items"""
+    file = models.FileField(upload_to='images')
+
+
+class Video (ItemBase):
+    """Video items"""
+    url = models.URLField()

cef48b300a3e35849b89cc7d5ae3f3ffb5e527d3
Ahmed Shahwan <ahmed@shahwan.me>
2019-03-14 18:45:58 +0200

    view course and module contents (using nested serializers)

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..models import Subject, Course, Module
+from ..models import Subject, Course, Module, Content
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -16,6 +16,36 @@ class ModuleSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     modules = ModuleSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules']
+
+
+# custom field
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ['order', 'item']
+
+
+class ModuleWithContentSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description', 'contents']
+
+
+class CourseWithContentSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentSerializer(many=True)
 
     class Meta:
         model = Course
